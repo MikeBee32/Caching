f3f425046e29aae987606aeb3a3ba8ad44a070af
Mike Beeson <michaelbeeson@gmail.com>
2021-04-14 19:00:16 +0100

    Initial commit

---
+++ Caching.CLI/DataCache`.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Specialized;
+using System.Runtime.Caching;
+
+namespace Caching.CLI
+{
+    public class DataCache<T>
+    {
+        private readonly object _lock = new object();
+        private readonly MemoryCache _cache;
+        private readonly TimeSpan _expiryTime;
+
+        public DataCache(
+            int cacheSize = 100,
+            int pollInterval = 5,
+            int expiryTime = 20)
+        {
+            var cacheSettings = new NameValueCollection
+            {
+                { "CacheMemoryLimitMegabytes", $"{cacheSize}" },
+                { "PollingInterval", $"{TimeSpan.FromMinutes(pollInterval):hh\\:mm\\:ss}" }
+            };
+            _cache = new MemoryCache(typeof(T).Name, cacheSettings);
+            _expiryTime = TimeSpan.FromMinutes(expiryTime);
+        }
+
+        public T GetOrAdd(int keyValue, Func<int, T> create)
+        {
+            lock (_lock)
+            {
+                var key = $"{keyValue}";
+                var cacheItem = _cache.GetCacheItem(key);
+
+                T value;
+
+                if (cacheItem == null)
+                {
+                    value = create(keyValue);
+                    cacheItem = new CacheItem(key, value);
+                    var expirationPolicy = new CacheItemPolicy { SlidingExpiration = _expiryTime };
+                    _cache.Add(cacheItem, expirationPolicy);
+                }
+                else
+                {
+                    value = (T)cacheItem.Value;
+                }
+
+                return value;
+            }
+        }
+    }
+}
---
+++ Caching.CLI/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Caching.CLI
+{
+    class Program
+    {
+        // Given a function f(x: int) -> int without side effects and whose
+        // output is only dependent on it's single parameter — please produce a
+        // function g(x: int) -> int which produces the same output as f for
+        // every value of x but uses caching to improve performance.
+
+        private static readonly DataCache<int> _cache = new DataCache<int>();
+
+        static void Main(string[] args)
+        {
+            for (var j = 0; j < 5; j++)
+            {
+                for (var i = 0; i < 5; i++)
+                {
+                    var value = g(i);
+
+                    Console.WriteLine($"f({i}) = {value}");
+                }
+            }
+        }
+
+        private static int f(int x)
+        {
+            return 2 * x;
+        }
+
+        private static int g(int x)
+        {
+            return _cache.GetOrAdd(x, x => f(x));
+        }
+    }
+}
